24cefbb5b261b3e62dea5b9a1f5dde22b7a87b8f
Olha Tkachova <olha.prozorro@gmail.com>
2019-02-14 11:57:16 +0200

    add rectangle1, 2

--- workWithSquare.js
+++ workWithSquare.js
@@ -6,3 +6,13 @@ var square = document.getElementById("square")
 square.onclick = function () {
     square.style.backgroundColor = 'rgb(' + getRandomInt(0, 255) + ', ' + getRandomInt(0, 255) + ', ' + getRandomInt(0, 255) + ')';
 };
+
+var rectangle1 = document.getElementById("rectangle1")
+rectangle1.onclick = function () {
+    rectangle1.style.backgroundColor = 'rgb(' + getRandomInt(0, 255) + ', ' + getRandomInt(0, 255) + ', ' + getRandomInt(0, 255) + ')';
+};
+
+var rectangle2 = document.getElementById("rectangle2")
+rectangle2.onclick = function () {
+    rectangle2.style.backgroundColor = 'rgb(' + getRandomInt(0, 255) + ', ' + getRandomInt(0, 255) + ', ' + getRandomInt(0, 255) + ')';
+};
